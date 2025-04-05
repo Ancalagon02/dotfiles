508f443ecd9214aff7d8f88df9eb227f7e1179b3
Wesley Peeters <peeterswap@protonmail.com>
2025-04-05 17:15:58 +0200

    changed some configs

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,45 +1,29 @@
-local keys = {
-  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find All Files" },
-}
-
-local config = function()
-  local telescope = require("telescope")
-  telescope.setup({
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-j>"] = "move_selection_next",
-          ["<C-k>"] = "move_selection_previous",
-        },
-      },
-    },
-    pickers = {
-      find_files = {
-        file_ignore_patterns = { "node_modules", ".venv" },
-        additional_args = function(_)
-          return { "--hidden", "--no-ignore-vcs" }
-        end,
-        no_ignore = true,
-        hidden = true,
-      },
-    },
-    extensions = {
-      "fzf",
-    },
-  })
-  telescope.load_extension("fzf")
-
-end
-
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     { "nvim-lua/plenary.nvim" },
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-    },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", },
   },
-  keys = keys,
-  config = config,
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
+          },
+        },
+      },
+      extensions = {
+        "fzf",
+      },
+    })
+
+    telescope.load_extension("fzf")
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+
+  end
 }
