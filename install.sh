41cac26dfa3cf6630ae6ed0597f2b927c6611e36
Ancalagon02 <peeterswap@protonmail.com>
2025-04-06 01:44:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ln -s $(pwd)/ghostty $HOME/.config/
 ln -s $(pwd)/hypr $HOME/.config/
-ln -s $(pwd)/dotfiles/nvim $HOME/.config/
+ln -s $(pwd)/nvim $HOME/.config/
 ln -s $(pwd)/zsh/.zshrc $HOME/.zshrc
 ln -s $(pwd)/zsh/.zshenv $HOME/.zshenv
 ln -s $(pwd)/ohmyposh $HOME/.config/
