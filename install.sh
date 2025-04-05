c9b15c54b119224883ab9bc96dc2d65b34d05d58
Wesley Peeters <peeterswap@protonmail.com>
2025-04-06 01:43:24 +0200

    oh my posh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s $(pwd)/hypr $HOME/.config/
 ln -s $(pwd)/dotfiles/nvim $HOME/.config/
 ln -s $(pwd)/zsh/.zshrc $HOME/.zshrc
 ln -s $(pwd)/zsh/.zshenv $HOME/.zshenv
+ln -s $(pwd)/ohmyposh $HOME/.config/
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -12,4 +12,4 @@ alias nas="cd /mnt/nas"
 
 source <(fzf --zsh)
 
-eval "$(oh-my-posh init zsh)"
+eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/config.toml)"
