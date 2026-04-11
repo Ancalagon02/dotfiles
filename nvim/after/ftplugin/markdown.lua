ba7c8ede346f56b4a937026dff1c655c4c2c97b8
Ancalagon <ancalagon102@gmail.com>
2026-04-11 21:32:07 +0200

    Update stuf

--- nvim/after/ftplugin/markdown.lua
+++
@@ -1,5 +0,0 @@
-local opt = vim.opt_local
-
-opt.textwidth = 100
-opt.spell = true
-opt.linebreak = true
--- nvim/after/ftplugin/python.lua
+++
@@ -1,5 +0,0 @@
-local opt = vim.opt_local
-
-opt.signcolumn = "yes"
-opt.colorcolumn = "120"
-
